f89f424fda577110b612f9f60293afa8f08aa80e
4RP3ggIO <35314095+4RP3ggIO@users.noreply.github.com>
2023-03-28 12:34:20 -0600

    Create gavin.anderson.nix

---
+++ roles/users/gavin.anderson.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, lib, ... }:
+
+let
+  username =  "gavin.anderson";
+in {
+  users.users."${username}" = {
+    description = "${username}";
+    home = "/Users/${username}";
+    name = "${username}";
+    shell = pkgs.zsh;
+    packages = [
+      pkgs.zsh
+      pkgs.gcc
+      pkgs.git
+      pkgs.gnupg
+    ];
+  };
+}
