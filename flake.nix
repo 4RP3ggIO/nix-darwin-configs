3c3387aad9ff5882e03925c634a2a250bc5d7f70
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-07-27 15:00:02 -0600

    fixed comment

--- flake.nix
+++ flake.nix
@@ -32,11 +32,11 @@
         specialArgs = inputs;
         modules = [ ./hosts/intel-macbook.nix ];
       };
-      # m1-macmini" = darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-        specialArgs = inputs;
-        modules = [ ./hosts/m1-macmini.nix ];
-      };
+      #"m1-macmini" = darwin.lib.darwinSystem {
+      #  system = "aarch64-darwin";
+      #  specialArgs = inputs;
+      #  modules = [ ./hosts/m1-macmini.nix ];
+      #};
     };
   };
 }
